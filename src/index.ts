e185000f3e7a6bb32a2e65ace31adbdd95421c73
Oscar Islas <oislasreyes@gmail.com>
2020-06-10 20:07:04 -0500

    Agregando multer al proyecto para subir archivos

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,30 @@ import env from 'dotenv';
 import cookieParser from 'cookie-parser';
 import csurf from 'csurf';
 import auth from './middleware/auth';
+import multer from 'multer';
+import path from 'path';
 
 const NODE_ENV = process.env.NODE_ENV; //Obteniendo el entorno de desarrollo 
 env.config({ path: `.env.${NODE_ENV}`}); //Cargamos el archivo de variables de entorno 
 
 const app = express();
-const csurfProtection = csurf({cookie: true}); //Token CSRF -> Cross-site Request Forgery 
+const csurfProtection = csurf({cookie: true}); //Token CSRF -> Cross-site Request Forgery
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, 'uploads'))
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        if(ext !== '.pdf'){
+            cb(null, file.originalname);
+        }else{
+            cb(new Error('No se permiten archivos con la extensión pdf'), '');
+        }
+    }
+});
+
+//Limitando la subida de archivos de más de 3MB
+const upload = multer({storage: storage, limits: {fileSize: 3096000}});
 
 //application/json
 app.use(bodyParser.json());
@@ -22,6 +40,18 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(cookieParser());
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.json({
+        file: req.file
+    });
+});
+
+app.post('/upload-many', upload.array('files', 5), (req, res) => {
+    res.json({
+        file: req.files
+    });
+});
 
 app.use(csurfProtection);
 
